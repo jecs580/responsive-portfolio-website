7bd1019338fbdb696800e6deb77b199d4d4a37f8
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-27 19:52:01 -0400

    Creacion de seccion 'qualitifation'

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,23 @@ skillsHeader.forEach((el)=>{
     el.addEventListener('click',toggleSkills)
 })
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target'),
+      tabsContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabsContents.forEach(tabsContents =>{
+            tabsContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__activte')
+        tab.forEach(tab=>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
