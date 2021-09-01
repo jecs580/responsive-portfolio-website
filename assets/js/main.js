2ef155f7cf1c9ce6635e3e004a4d4d65ee1e2935
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-31 22:41:51 -0400

    Section 'Testimonial'

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,7 +83,7 @@ modalCloses.forEach((modalClose)=>{
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper =  new Swiper('.portfolio__container',{
+let swiperPortfolio =  new Swiper('.portfolio__container',{
     cssMode: true,
     loop: true,
 
@@ -98,6 +98,21 @@ let swiper =  new Swiper('.portfolio__container',{
 })
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial =  new Swiper('.testimonial__container',{
+    loop: true,
+    grabCursor:true,
+    spaceBetween:48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dinamicBullets:true,
+    },
+    breakpoints: {
+        568:{
+            slidesPreView:2,
+        }
+    }
+})
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
