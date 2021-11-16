af472387535597afc9b158685fa88fcf7f323472
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-11-15 21:58:57 -0400

    Cambios en el CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,3 +173,14 @@ themeButton.addEventListener('click',()=>{
     localStorage.setItem('selected-theme',getCurrentTheme())
     localStorage.setItem('selected-icon',getCurrentIcon())
 })
+
+// sending email
+
+const $form = document.querySelector('#form')
+$form.addEventListener('submit',handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const form  = new FormData(this);
+    console.log(form.get('name'));
+}
